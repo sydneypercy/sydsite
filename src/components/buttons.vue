d85d8739162021ae8c0324fc0e77ed942048b3c1
Sydney Percy <sydneyabby@cumallover.me>
2026-02-25 03:13:44 +0700

    separate the buttons and navbar

---
+++ src/components/buttons.vue
@@ -0,0 +1,18 @@
+<template>
+		<h3>cool people</h3>
+	<div>
+		<a href="https://kraafter.me/">
+			<img src="https://kraafter.me/assets/img/button.png" alt="Kraafter.me button" title="kraaftersite">
+		</a>
+		<a href="https://bearodactyl.dev">
+			<img src="../assets/bearo.gif" />
+		</a>
+		<a href="https://rooot.gay" title="rooot :3">
+          <img width="88" height="31" src="https://rooot.gay/assets/img/88x31/rooot.gif" alt="image with 2 white-pink kitten paws, one holding a trans flag. below is a trans-colored text: rooot.gay~">
+        </a>
+		<a href="https://malice.works">
+			<img  src="../assets/malice.png" alt="malice.works">
+		</a>
+	</div>
+
+</template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+	<ul>
+		<li><RouterLink to="/"> Home </RouterLink></li>
+		<li><RouterLink to="/about"> About </RouterLink></li>
+		<li><RouterLink to="/socials"> Socials </RouterLink></li>
+	</ul>
+</template>
+
+<style scoped></style>
